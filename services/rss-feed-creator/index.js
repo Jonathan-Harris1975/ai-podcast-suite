90f929c46f9dcfe5d90451d35c0ee1d00f0e1323
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-07 03:57:41 +0100

    Update index.js

--- services/rss-feed-creator/index.js
+++ services/rss-feed-creator/index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import cors from "cors";
 import { log } from "../../utils/logger.js";
-import routes from "./routes/index.js.js";
+import routes from "./routes/index.js";
 
 const app = express();
 app.use(cors());
