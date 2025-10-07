d8be969680d555af64f5be593113e6037d282cf9
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-07 05:18:11 +0100

    Add files via upload

---
+++ services/rss-feed-creator/utils/index.js
@@ -0,0 +1,16 @@
+// services/rss-feed-creator/index.js
+import express from "express";
+import cors from "cors";
+import { log } from "./utils/logger.js";
+import routes from "./routes/index.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/", routes);
+
+log.info("✅ rss-feed-creator env OK");
+log.info("🚀 RSS Feed Creator Service initialized");
+
+export default app;
