9153ded3b95d2225adb1bc61eb6d39276317de1c
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-09 21:57:02 +0100

    Add files via upload

--- services/script/app.js
+++ services/script/app.js
@@ -1,3 +1,4 @@
+import { s3, R2_BUCKETS, validateR2Once, uploadBuffer, listKeys, getObjectAsText } from "../r2-client.js";
 import express from 'express';
 import composeRouter from './routes/compose.js'; // Adjust if your path is different
 
--- services/script/index.js
+++ services/script/index.js
@@ -1,3 +1,4 @@
+import { s3, R2_BUCKETS, validateR2Once, uploadBuffer, listKeys, getObjectAsText } from "../r2-client.js";
 import { log } from "../../utils/logger.js";
 export async function runScript({ sessionId, nowISO }) {
   log.info({ sessionId, nowISO }, "📝 [Script] start");
