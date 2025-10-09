af241cad8a7352211362b622d5dfa257a5d87116
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-09 23:59:50 +0100

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import { s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText } from "../../r2-client.js";
+// services/rss-feed-creator/routes/index.js
+import express from "express";
+import { log } from "../../../utils/logger.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  log.info("📰 RSS Feed Creator root route hit");
+  res.json({ ok: true, service: "rss-feed-creator" });
+});
+
+export default router;
