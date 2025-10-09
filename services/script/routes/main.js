4c60aeab0b6f6ab9fdfa22476abef02b5baeb2be
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-10 00:25:17 +0100

    Add files via upload

--- services/script/routes/main.js
+++ services/script/routes/main.js
@@ -1,4 +1,4 @@
-import { s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText } from "../../r2-client.js";
+import {s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText} from "../../r2-client.js";
 // routes/main.js
 import express from 'express';
 import fs from 'fs';
