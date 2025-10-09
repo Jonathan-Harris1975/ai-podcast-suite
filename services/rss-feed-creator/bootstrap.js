8e54544d7f24942a22f6246a780ce45fac405e13
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-09 23:58:21 +0100

    Add files via upload

--- services/rss-feed-creator/bootstrap.js
+++ services/rss-feed-creator/bootstrap.js
@@ -1,4 +1,4 @@
-import { s3, R2_BUCKETS, validateR2Once, uploadBuffer, listKeys, getObjectAsText } from "../r2-client.js";
+import { s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText } from "../r2-client.js";
 // services/rss-feed-creator/bootstrap.js
 import fs from "fs/promises";
 import path from "path";
