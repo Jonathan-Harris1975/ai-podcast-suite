89543b7b18e428ec900d2f70000e82e8adebb946
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-09 22:02:25 +0100

    Add files via upload

--- services/artwork/index.js
+++ services/artwork/index.js
@@ -1,3 +1,4 @@
+import { s3, R2_BUCKETS, validateR2Once, uploadBuffer, listKeys, getObjectAsText } from "../r2-client.js";
 import { log } from "../../utils/logger.js";
 export async function runArtwork({ sessionId }) {
   log.info({ sessionId }, "🎨 [Artwork] start");
