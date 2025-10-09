49fced1e04506015cdfc8bbc234f4f4efa169456
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-09 22:03:35 +0100

    Add files via upload

--- services/artwork/routes/generateArtwork.js
+++ services/artwork/routes/generateArtwork.js
@@ -1,3 +1,4 @@
+import { s3, R2_BUCKETS, validateR2Once, uploadBuffer, listKeys, getObjectAsText } from "../../r2-client.js";
 // routes/generateArtwork.js
 import express from "express";
 import fetch from "node-fetch";
