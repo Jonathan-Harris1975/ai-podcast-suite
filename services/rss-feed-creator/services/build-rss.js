6a20231dfeb07932518382c7e05a39b3b505785d
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-10 00:26:56 +0100

    Add files via upload

--- services/rss-feed-creator/services/build-rss.js
+++ services/rss-feed-creator/services/build-rss.js
@@ -1,4 +1,4 @@
-import { s3, R2_BUCKETS, validateR2Once, uploadBuffer, listKeys, getObjectAsText } from "../../r2-client.js";
+import {s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText} from "../../r2-client.js";
 // services/build-rss.js
 import { log } from "../../../utils/logger.js";
 import { putText } from "../utils/r2-client.js"; // ✅ fixed import
