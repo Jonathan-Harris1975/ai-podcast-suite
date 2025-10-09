bffa8eb14ed462b67bc4ed683f20059234e51e6d
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-10 00:29:08 +0100

    Add files via upload

--- services/artwork/routes/index.js
+++ services/artwork/routes/index.js
@@ -1,4 +1,4 @@
-import { s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText } from "../../r2-client.js";
+import {s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText} from "../../r2-client.js";
 
 // apps/artwork/routes/index.js
 import express from "express";
