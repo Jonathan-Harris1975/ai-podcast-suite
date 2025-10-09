9769019b26870449adf4ce69bcc294efbbba9f46
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-09 21:58:39 +0100

    Add files via upload

--- services/script/routes/createScript.js
+++ services/script/routes/createScript.js
@@ -1,3 +1,4 @@
+import { s3, R2_BUCKETS, validateR2Once, uploadBuffer, listKeys, getObjectAsText } from "../../r2-client.js";
 // routes/createScript.js
 import express from "express";
 import fetch from "node-fetch";
