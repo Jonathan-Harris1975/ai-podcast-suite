064d0153c387dee410d3c8ff6062b72db8c67613
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-11 01:22:37 +0100

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,6 @@
-// Minimal JSON logger
+// /utils/logger.js
 export const log = {
-  info: (msg, meta) => process.stdout.write(JSON.stringify({ time: new Date().toISOString(), message: msg, meta }) + "\n"),
-  debug: (msg, meta) => process.stdout.write(JSON.stringify({ time: new Date().toISOString(), message: msg, meta }) + "\n"),
-  error: (msg, meta) => process.stdout.write(JSON.stringify({ time: new Date().toISOString(), message: msg, meta }) + "\n"),
+  info: (msg, meta) => process.stdout.write(JSON.stringify({ level: "info", time: new Date().toISOString(), message: msg, ...(meta?{meta}: {}) }) + "\n"),
+  error: (msg, meta) => process.stdout.write(JSON.stringify({ level: "error", time: new Date().toISOString(), message: msg, ...(meta?{meta}: {}) }) + "\n"),
+  debug: (msg, meta) => process.stdout.write(JSON.stringify({ level: "debug", time: new Date().toISOString(), message: msg, ...(meta?{meta}: {}) }) + "\n"),
 };
