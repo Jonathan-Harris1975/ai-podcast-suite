608ee27025542c90addc85a39f1994a6209653c5
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-15 03:42:44 +0100

    Update podcastProcessor.js

--- services/tts/utils/podcastProcessor.js
+++ services/tts/utils/podcastProcessor.js
@@ -4,10 +4,8 @@ import path from "path";
 import os from "os";
 import { spawn } from "child_process";
 import fetch from "node-fetch";
-import { validateEnv } from "../services/env-checker.js";
-validateEnv();          // hard-stop if any env var is missing
-// single HeadBucket probe (no retries/ping)
-import logger from "./logger.js";
+
+import logger from "../shared/utils/logger.js";
 
 const tempDir = os.tmpdir();
 
