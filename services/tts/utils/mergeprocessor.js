d9909ad2c97ba8aed76e4693abf5cb9449b53959
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-15 03:45:18 +0100

    Update mergeprocessor.js

--- services/tts/utils/mergeprocessor.js
+++ services/tts/utils/mergeprocessor.js
@@ -5,7 +5,7 @@ import path from "path";
 import { spawn } from "child_process";
 import fetch from "node-fetch";
 
-import logger from "../shared/utils/logger.js";
+import logger from "../../../utils/logger.js";
 
 const tempDir = "/tmp"; // Render ephemeral storage
 
